09b9c8cc76989f9ba26f1a731fc26271497c0a18
KyeongMinBae <reaaaaaaal@gmail.com>
2026-04-04 17:34:37 +0900

    15. 파일 입출력과 성능 최적화1 - 하나씩 쓰기

---
+++ src/io/buffered/BufferedConst.java
@@ -0,0 +1,9 @@
+package io.buffered;
+
+public class BufferedConst {
+
+    public static final String FILE_NAME = "temp/buffred.dat";
+    public static final int FILE_SIZE = 10 * 1024 * 1024; //10MB
+    public static final int BUFFER_ISZE = 8192; // 8KB
+
+}
